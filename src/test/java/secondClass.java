c4f693e8222b4a9d7a89b50c1d74123cee74fc60
hkm66 <hikmet@cydeo.com>
2023-04-21 22:07:23 +0300

    commit fff

--- src/test/java/secondClass.java
+++ src/test/java/secondClass.java
@@ -2,5 +2,6 @@ public class secondClass {
     public static void main(String[] args) {
         System.out.println("selamlar");
         System.out.println("meryem");
+        System.out.println("filizzzz");
     }
 }
