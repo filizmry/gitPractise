8aa9f9e67c08d2a1406350beeeb6f4db46a7426d
hkm66 <hikmet@cydeo.com>
2023-04-21 21:41:13 +0300

    commit hkm3

--- src/test/java/firstClass.java
+++ src/test/java/firstClass.java
@@ -2,6 +2,6 @@ public class firstClass {
     public static void main(String[] args) {
         System.out.println("merhaba");
         System.out.println("mry");
-        System.out.println("hi");
+        System.out.println("hii");
     }
 }
